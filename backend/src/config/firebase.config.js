ab4c3dac90f783b5ed118ac4aaf463da45e4ce2d
Vyshnav C J <vyshnavjenilkumar@gmail.com>
2023-06-09 13:35:27 +0530

    refactor(config): config file fixed

--- backend/src/config/firebase.config.js
+++ backend/src/config/firebase.config.js
@@ -1,10 +1,10 @@
 module.exports.firebaseConfig = {
-  apiKey: 'AIzaSyDwut6jASd0zJVNuMU7ADkBYvak54FOFJQ',
-  authDomain: 'blud-back-end.firebaseapp.com',
-  databaseURL: 'https://blud-back-end-default-rtdb.firebaseio.com',
-  projectId: 'blud-back-end',
-  storageBucket: 'blud-back-end.appspot.com',
-  messagingSenderId: '1042004075278',
-  appId: '1:1042004075278:web:93b7f7affa62e3a2ebed5f',
-  measurementId: 'G-8MMNHW44J7'
+  apiKey: process.env.FIRE_API_KEY,
+  authDomain: process.env.FIRE_DOMIN,
+  databaseURL: process.env.FIRE_DB_URL,
+  projectId: process.env.FIRE_PROJECT_ID,
+  storageBucket: process.env.FIRE_STORAGE,
+  messagingSenderId: process.env.FIRE_MESSAGE_ID,
+  appId: process.env.FIRE_APP_ID,
+  measurementId: process.env.FIRE_MEASUREMENT_ID
 };
