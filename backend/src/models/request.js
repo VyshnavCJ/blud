f84f4dbea44069cad2c25aba031e4ba4e5061d86
Vyshnav C J <vyshnavjenilkumar@gmail.com>
2023-06-17 18:20:05 +0530

    fix: model fix

--- backend/src/models/request.js
+++ backend/src/models/request.js
@@ -80,7 +80,7 @@ const RequestSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-RequestSchema.index({ location: '2dsphere' });
+RequestSchema.index({ Point: '2dsphere' });
 RequestSchema.pre('save', async function (next) {
   if (!this.isModified('pinCode')) return;
   const loc = await geocoder.geocode(this.pinCode);
